962ab446871468f670a8eab4afc2ad7e093a5bb1
Ian Chee <ian.chee@arete.ltd>
2022-10-04 21:23:28 -0700

    Constructing your Chain: added requestMessageOwnershipVerification() in blockchain.js

--- src/blockchain.js
+++ src/blockchain.js
@@ -86,7 +86,9 @@ class Blockchain {
      */
     requestMessageOwnershipVerification(address) {
         return new Promise((resolve) => {
-            
+            let msgToSign = `${address}:${new Date().getTime().toString().slice(0,-3)}:starRegistry`;
+            console.log(msgToSign);
+            resolve(msgToSign);
         });
     }
 
